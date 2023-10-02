6e263678d699fe7219e0b0506b42a174d76c7956
Jose Jorge <j.jorgem.s01@hotmail.com>
2023-10-02 07:53:58 -0600

    Proyecto kick off

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100"></bounce-loader>
+        <px-assets-table v-if="!isLoading" :assets="assets"/>
+    </div>
+</template>
+
+<script>
+import api from '@/api.js'
+import PxAssetsTable from "@/components/PxAssetsTable.vue";
+export default {
+    name: 'MyHome',
+    components: {
+        PxAssetsTable
+    },
+    data(){
+        return {
+            assets: [],
+            isLoading: false
+        }
+    },
+    created(){
+        this.isLoading = true;
+        api.getAssets()
+        .then( data => this.assets = data)
+        .finally(() => this.isLoading = false );
+    }
+}
+</script>
